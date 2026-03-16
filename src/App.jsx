6f85e16d4be5dec77f4da756e3276fb1dc0272ee
saniyaaryan7 <aryansaniya7@gmail.com>
2026-03-17 00:45:07 +0530

    Add lazy loading and Suspense for route components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import React, { Suspense } from 'react'
+
 import './App.css'
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
 import Navbar from './Components/Webpage/Navbar/Navbar'
 import Footer from './Components/Webpage/Footer/Footer'
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Services from './pages/Services'
+const Home = React.lazy(() => import( "./pages/Home"));
+const About = React.lazy(() => import( "./pages/About"));
+const Contact = React.lazy(() => import( "./pages/Contact"));
+const Services = React.lazy(() => import( "./pages/Services"));
+
+
+
+
+// import About from './pages/About'
+// import Contact from './pages/Contact'
+// import Services from './pages/Services'
 
 function App() {
   
@@ -16,13 +25,16 @@ function App() {
       
       <BrowserRouter>
       <Navbar/> 
+
+<Suspense fallback={<h1>Loading...</h1>}>
+
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/About' element={<About />}/>
         <Route path='/Services' element={<Services />}/>
         <Route path='/Contact' element={<Contact />}/>
       </Routes>
-
+</Suspense>
       <Footer/>   
       </BrowserRouter>
     </>
